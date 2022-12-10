28f6bee74cbd1eab3776aae4055d850b278340d8
AKANSHA <gautamakansha312@gmail.com>
2022-12-10 19:30:41 +0530

    Splash Logo

--- app/src/main/java/android/example/hoysalaelectronics/Adapters/HorizontalRecycler.kt
+++ app/src/main/java/android/example/hoysalaelectronics/Adapters/HorizontalRecycler.kt
@@ -1,4 +1,28 @@
 package android.example.hoysalaelectronics.Adapters
 
-class HorizontalRecycler {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class HorizontalRecycler : RecyclerView.Adapter<HorizontalRecycler.HorizontalViewHolder>() {
+
+    class HorizontalViewHolder(view : View):RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HorizontalViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: HorizontalViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
+
 }
