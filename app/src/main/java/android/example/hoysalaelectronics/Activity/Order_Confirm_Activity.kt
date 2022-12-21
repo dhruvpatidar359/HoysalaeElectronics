872d7ac4cf8edc48fffe2172c0ed8265e4b7064f
NaveenSharma07 <naveenSharma4325@gmail.com>
2022-12-21 11:26:27 +0530

    Order confirm activity added and Error resolved.(19-12-2022)

---
+++ app/src/main/java/android/example/hoysalaelectronics/Activity/Order_Confirm_Activity.kt
@@ -0,0 +1,12 @@
+package android.example.hoysalaelectronics.Activity
+
+import android.example.hoysalaelectronics.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Order_Confirm_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_confirm)
+    }
+}
--- app/src/main/java/android/example/hoysalaelectronics/Activity/PaymentMethod.kt
+++ app/src/main/java/android/example/hoysalaelectronics/Activity/PaymentMethod.kt
@@ -1,8 +1,11 @@
 package android.example.hoysalaelectronics.Activity
 
+import android.content.Intent
 import android.example.hoysalaelectronics.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
 import android.widget.Toolbar
 
 class PaymentMethod : AppCompatActivity() {
@@ -17,5 +20,42 @@ class PaymentMethod : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setHomeButtonEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+    fun onCheckboxClicked(view: View) {
+        if (view is CheckBox) {
+            val checked: Boolean = view.isChecked
+
+            when (view.id) {
+                R.id.cb_pay_paypal -> {
+                    if (checked) {
+                       val Intent = Intent(this@PaymentMethod,Order_Confirm_Activity::class.java)
+                       startActivity(Intent)
+                    } else {
+
+                    }
+                }
+                R.id.cb_razor_pay -> {
+                    if (checked) {
+                        val Intent = Intent(this@PaymentMethod,Order_Confirm_Activity::class.java)
+                        startActivity(Intent)
+                    } else {
+
+                    }
+                }
+                R.id.cb_cash -> {
+                    if (checked) {
+                        val Intent = Intent(this@PaymentMethod,Order_Confirm_Activity::class.java)
+                        startActivity(Intent)
+                    } else {
+
+                    }
+                }
+            }
+        }
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
